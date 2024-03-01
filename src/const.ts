f063b7b62c97510dd996710478c46bab623d2484
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2024-03-01 22:47:25 +0300

    fix: revert origin

--- src/const.ts
+++ src/const.ts
@@ -2,7 +2,7 @@ export const cardStyle = {
   boxShadow: "1px 1px 10px",
 }
 
-const rootUrl = "http://127.0.0.1:8000/api/v1"
+const rootUrl = "http://localhost:8000/api/v1"
 
 export const urls = {
   solveKinetics: `${rootUrl}/reaction_kinetics_solver/`,
