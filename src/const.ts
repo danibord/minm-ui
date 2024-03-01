09f2d932149f037c99a884df7cd798cc046ab122
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2024-03-01 22:51:04 +0300

    fix: revert slash

--- src/const.ts
+++ src/const.ts
@@ -5,5 +5,5 @@ export const cardStyle = {
 const rootUrl = "http://localhost:8000/api/v1"
 
 export const urls = {
-  solveKinetics: `${rootUrl}/reaction_kinetics_solver/`,
+  solveKinetics: `${rootUrl}/reaction_kinetics_solver`,
 }
