8da14d07fcb336dfa5f299f1f19bcf9ec0d87798
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2024-03-06 21:43:14 +0300

    feat: add charts legend

--- src/components/ResultSection/ResultSection.tsx
+++ src/components/ResultSection/ResultSection.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react"
+import { memo, useMemo } from "react"
 import { CalculationResult } from "../../types"
 import { Card } from "antd"
 import { cardStyle } from "../../const"
@@ -9,20 +9,36 @@ interface ResultSectionProps {
 }
 
 export const ResultSection = memo(({ value }: ResultSectionProps) => {
-  const data: { time: number; value: number; component: string }[] = []
-  value.kinetics_ODE_solution.forEach((values, timeIndex) => {
-    const time = value.time_array[timeIndex]
-    values.forEach((value, index) => {
-      data.push({ time, value, component: `Component ${index + 1}` })
+  const data = useMemo(() => {
+    const result: { time: number; value: number; component: string }[] = []
+    value.kinetics_ODE_solution.forEach((values, timeIndex) => {
+      const time = value.time_array[timeIndex]
+      values.forEach((value, index) => {
+        result.push({ time, value, component: `Component ${index + 1}` })
+      })
     })
-  })
-  const config = {
-    data,
-    xField: "time",
-    yField: "value",
-    legend: { size: false },
-    colorField: "component",
-  }
+    return result
+  }, [value])
+
+  const config = useMemo(
+    () => ({
+      data,
+      xField: "time",
+      yField: "value",
+      shapeField: "smooth",
+      axis: {
+        x: { title: "Время, сек", size: 40 },
+        y: { title: "Концентрация, моль/л", size: 36 },
+      },
+      slider: {
+        x: { labelFormatter: "~s" },
+        y: { labelFormatter: "~s" },
+      },
+      colorField: "component",
+    }),
+    [data],
+  )
+
   return (
     <Card title="Результат" style={cardStyle}>
       <Line {...config} />
