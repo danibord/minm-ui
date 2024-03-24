e8bc760cc66ec58308302ed7de1398396bdc6dc0
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2024-03-24 20:35:28 +0300

    chore: remove unused variables

--- src/const.ts
+++ src/const.ts
@@ -1,11 +1,5 @@
 import { METHOD } from "./types"
 
-export const commonGap = 16
-
-export const cardStyle = {
-  boxShadow: "1px 1px 10px",
-}
-
 const rootUrl = "http://localhost:8000/api/v1"
 
 export const urls = {
